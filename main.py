39a6099a42535f8ebd113918e320245c5959ed91
Cheese <chizenge1166@gmail.com>
2026-06-30 07:36:05 +0000

    added bubble sort

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ def bubble_sort(arr: list) -> None:
     Args:
         arr: list to be sorted (modified in-place)
     """
-    # Replace the code below with your implementation
-    raise NotImplementedError
-
+    if len(arr) <= 1:
+        return arr
+    
+    for i in range (len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                value = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = value
+    return arr
 
 def insertion_sort(arr: list) -> None:
     """Sort the array using insertion sort (in-place).
